127d9f2c43e59ad1686b6a56a4aa2f8ee045e2d6
Bryan Thetford <bryan.thetford@gmail.com>
2018-03-14 22:58:42 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,18 @@ app.post('/todos', (req, res) => {
   })
 });
 
+
+app.get('/todos', (req, res) => {
+  console.log('getting todos');
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    console.log('error', e);
+    res.status(400).send(e);
+  })
+})
+
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
