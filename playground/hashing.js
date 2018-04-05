16758802183c3622ebda3c4ae9908563e03e649a
Bryan Thetford <bryan.thetford@gmail.com>
2018-04-04 22:52:16 -0500

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,19 @@
 const {SHA256} = require('crypto-js');
 const  jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (error, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$eieLZXmGuYsYRTA0KSfKV.XsGEvTEDaHj.npMbxYXQ8Es3AS7B1BO';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 
 // takes the data and signs it; creating the hash
 //and returns the token
@@ -8,7 +22,7 @@ var data = {
   id: 10
 }
 var token = jwt.sign(data, '123abc');
-var decoded = jwt.verify(token, '123abcc');
+var decoded = jwt.verify(token, '123abc');
 
 
 // var message = 'I am user number 3';
