54b1dc51ed063921dabdc851c81bd61888ebafb9
Bryan Thetford <bryan.thetford@gmail.com>
2018-04-06 13:21:50 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,27 @@ app.post('/users', (req, res) => {
 });
 
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    //create new token
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+})
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+
+
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -123,11 +144,6 @@ app.patch('/todos/:id', (req, res) => {
 
 })
 
-
-
-app.get('/users/me', authenticate, (req, res) => {
-    res.send(req.user);
-});
 
 
 app.listen(port, () => {
